bc7ca0cecfdb1425ef18fcc1e901fe9c7f07231a
yaokel25 <114490360+yaokel25@users.noreply.github.com>
2024-12-04 21:45:11 -0500

    Update Browse.js

--- frontend/src/pages/Browse.js
+++ frontend/src/pages/Browse.js
@@ -17,17 +17,7 @@ function Browse() {
   return (
     <div className='Browse' style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
      <h1>Please Select School/Department/Course/Unit</h1>
-     <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
-     /*
+     
       <div className='dd-content'>
 
       
